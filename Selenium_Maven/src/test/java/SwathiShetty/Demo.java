d1cd47c5416ac59971708b0d06941b42e09ea7bd
08swa <08swa@96636880978660>
2025-04-05 15:09:20 +0530

    Demo class added

---
+++ Selenium_Maven/src/test/java/SwathiShetty/Demo.java
@@ -0,0 +1,12 @@
+package SwathiShetty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+     @Test
+     
+     public void sample() {
+    	 Reporter.log("Sample class added");
+     }
+}
